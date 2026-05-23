9099abb2bd6e96f23077847744402295269d16f8
Trauzer <65812855+Trauzer@users.noreply.github.com>
2026-05-23 03:24:31 +0200

    Refactor UNIT_Chetos component to enhance player outlining with faction-based colors and improve player registration handling

--- UnitClan/Scripts/Game/Chetos/Chetos.c
+++ UnitClan/Scripts/Game/Chetos/Chetos.c
@@ -3,25 +3,53 @@ class UNIT_ChetosClass : ScriptComponentClass
 {
 }
 
-bool commandSwitch = false;
-bool keySwitch = false;
+bool commandSwitch = false; //TODO: Move it to class
 
 class UNIT_Chetos : ScriptComponent
 {
-	protected ref array<IEntity> m_Characters;
-	protected ref Color m_OutlineColor = Color.FromRGBA(255, 0, 0, 255);
-	protected string m_sIdentityWhitelist = "07451a2b-0679-4175-a94e-203e9f49cc97";
+	protected ref map<IEntity, bool> m_Characters;
+	protected ref Color m_OutlineEnemyColor = Color.FromRGBA(255, 0, 0, 255);
+	protected ref Color m_OutlineFriendlyColor = Color.FromRGBA(0, 0, 255, 255);
+	protected string m_sIdentityWhitelist = "07451a2b-0679-4175-a94e-203e9f49cc97"; //TODO: Permission/whitelist system
+	protected bool keySwitch = false;
+	protected Faction m_PlayerFaction;
+
 	
-	/*protected Shape CreateShapeOnCharacter(IEntity entity) 
-	{
-		return ShowBoneDebug(entity, entity.GetBoneIndex("Hips", 1.0));
-	}*/
+	override void OnPostInit(IEntity owner)
+	{	
+		super.EOnActivate(owner);
+			
+		Game game = GetGame();
+		
+		if (game.InPlayMode())
+		{
+			UUID localIdentity = BackendAuthenticatorApi.GetIdentityId();
+			if (localIdentity != m_sIdentityWhitelist)
+				return;
+		}
+		//TODO: Admin check		
+		
+		SCR_BaseGameMode gm = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+    	if (gm)
+        	gm.GetOnPlayerRegistered().Insert(OnPlayerRegistered);
+			
+		game.GetInputManager().AddActionListener("RHS_CharacterOpenRadial", EActionTrigger.DOWN, KeyPress); //Change dependency
+		
+		GenericEntity.Cast(owner).Activate();
+		m_Characters = new map<IEntity, bool>();
+		SetEventMask(owner, EntityEvent.FRAME);
+	}
 	
 	override void EOnFrame(IEntity owner, float timeSlice)
 	{
 		super.EOnFrame(owner, timeSlice);
 		
 		if (commandSwitch != true) 
+		{
+			return;
+		}
+		
+		if (keySwitch != true)
 		{
 			return;
 		}
@@ -47,7 +75,7 @@ class UNIT_Chetos : ScriptComponent
 		
 		//world.GetActiveEntities(m_Characters);
 		
-		foreach (IEntity entity : m_Characters) 
+		foreach (IEntity entity, bool isFriendly : m_Characters) 
 		{
 			ChimeraCharacter character = ChimeraCharacter.Cast(entity);
 			if (!character)
@@ -56,7 +84,14 @@ class UNIT_Chetos : ScriptComponent
 			if (playerManager.GetPlayerIdFromControlledEntity(character) > 0)
 				continue;
 			
-			world.OutlineEntity(character, m_OutlineColor, 1.0, 0.25, true);
+			if (isFriendly) 
+			{
+				world.OutlineEntity(character, m_OutlineFriendlyColor, 1.0, 0.25, true);
+			}
+			else
+			{
+				world.OutlineEntity(character, m_OutlineEnemyColor, 1.0, 0.25, true);
+			}
 		}
 	}
 	
@@ -67,25 +102,77 @@ class UNIT_Chetos : ScriptComponent
 		
 		IEntity mainParent = SCR_EntityHelper.GetMainParent(e, true);
 		ChimeraCharacter character = ChimeraCharacter.Cast(mainParent);
-		
+
 		if (character && !m_Characters.Contains(character))
-			m_Characters.Insert(character);
+		{
+			bool isFriendly = true;
+
+			Faction faction = SCR_Faction.GetEntityFaction(character);
+			//SCR_FactionAffiliationComponent factionComp = SCR_FactionAffiliationComponent.Cast(character.GetCharacterController().FindComponent(SCR_FactionAffiliationComponent));
+			//Faction faction = factionComp.GetAffiliatedFaction();
+			
+			if (m_PlayerFaction.IsFactionEnemy(faction))
+			{
+				isFriendly = false;
+			}
+				
+			m_Characters.Insert(character, isFriendly);
+		}
 
 		return true;
 	}
 	
-	protected override void OnPostInit(IEntity owner)
-	{	
-		if (GetGame().InPlayMode())
+	protected void OnPlayerRegistered(int playerId)
+	{
+	    int localId = SCR_PlayerController.GetLocalPlayerId();
+	    if (playerId != localId)
+	        return;
+	
+	    SCR_PlayerController pc = SCR_PlayerController.Cast(GetGame().GetPlayerManager().GetPlayerController(playerId));
+	    if (!pc)
+	        return;
+	
+	    SCR_PlayerFactionAffiliationComponent m_PlayerFactionComp = SCR_PlayerFactionAffiliationComponent.Cast(pc.FindComponent(SCR_PlayerFactionAffiliationComponent));
+	    if (!m_PlayerFactionComp)
+	        return;
+	
+	    m_PlayerFaction = m_PlayerFactionComp.GetAffiliatedFaction();
+	    m_PlayerFactionComp.GetOnPlayerFactionChangedInvoker().Insert(OnPlayerFactionChanged);
+	
+	    // opcjonalnie: odpinamy sie od OnPlayerRegistered po pierwszym trafieniu
+	    SCR_BaseGameMode gm = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+	    if (gm)
+	        gm.GetOnPlayerRegistered().Remove(OnPlayerRegistered);
+	}
+	
+	protected void OnPlayerFactionChanged(SCR_PlayerFactionAffiliationComponent comp, Faction previous, Faction current)
+	{
+		Print("[UNIT]" + current.GetFactionIdentity(), LogLevel.NORMAL);
+	    m_PlayerFaction = current;
+	}
+	
+	void KeyPress(float value, EActionTrigger reason)
+	{
+		if (!keySwitch)
 		{
-			UUID localIdentity = BackendAuthenticatorApi.GetIdentityId();
-			if (localIdentity != m_sIdentityWhitelist)
-				return;
+			keySwitch = true;
+		} 
+		else
+		{
+			keySwitch = false;
 		}
+	}
+	
+	void ~UNIT_Chetos() 
+	{
+		GetGame().GetInputManager().RemoveActionListener("RHS_CharacterOpenRadial", EActionTrigger.DOWN, KeyPress);
 		
-		GenericEntity.Cast(owner).Activate();
-		m_Characters = {};
-		SetEventMask(owner, EntityEvent.FRAME);
+		SCR_BaseGameMode gm = SCR_BaseGameMode.Cast(GetGame().GetGameMode());
+	    if (gm)
+	        gm.GetOnPlayerRegistered().Remove(OnPlayerRegistered);
+	
+	    //if (m_PlayerFactionComp)
+	    //    m_PlayerFactionComp.GetOnPlayerFactionChangedInvoker().Remove(OnPlayerFactionChanged);
 	}
 }
 
